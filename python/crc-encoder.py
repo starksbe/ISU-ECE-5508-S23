7eccb9a00cdb6028365c55b050ffa999f1d705ad
Barrett Durtschi <durtbarr@isu.edu>
2023-04-02 19:44:26 -0600

    Print statements added for CRC before and after the XOR function, the Polynomial would not print because I found an issue that the the statement that XORs with the polynomial function is never entered with the current data.

--- python/crc-encoder.py
+++ python/crc-encoder.py
@@ -21,20 +21,32 @@ def crc_16_encode(data: int, poly: int = 0x8005) -> int:
     """
     # The initial value of the CRC register
     crc = 0x0000
-
+    
+   
     # Iterate over each bit of the data word
     for i in range(16):
         # If the current bit of the CRC register is 1
         if crc & 0x8000:
             # XOR the CRC register with the polynomial
+            #print(f"CRC:    {bin(crc)}") 
+            #current hardcode never enters this for loop
+           # print(f"Poly:    {bin(poly)}") 
             crc = (crc << 1) ^ poly
+            
+            #these messages are going to be print in binary as most people in class probably can read it easier
+            
         else:
             # Shift the CRC register to the left
             crc = crc << 1
+            #print(f"Shifted CRC:    {bin(crc)} \n" )
+
         # If the current bit of the data word is 1
         if data & (1 << (15-i)):
+            print(f"Data:           {bin(data)}")  
             # XOR the CRC register with the data word
-            crc = crc ^ 0x0001
+            print(f"Shifted CRC:    {bin(crc)} \n")
+            crc = crc ^ 0x0001 ##### Why is this set to 1? Shouldn't it be data?
+            print(f"CRC after XOR:    {bin(crc)} \n")
 
     # Return the final value of the CRC register as a 16-bit integer
     return crc
