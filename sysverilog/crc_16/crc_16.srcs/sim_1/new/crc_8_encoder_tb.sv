b0102bc04f4242212772f13809b10ba0f8139a1d
Barrett Durtschi <durtbarr@isu.edu>
2023-04-23 14:36:34 -0600

    Initial source files for 8bit crc encoder

---
+++ sysverilog/crc_16/crc_16.srcs/sim_1/new/crc_8_encoder_tb.sv
@@ -0,0 +1,50 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/11/2023 09:19:13 AM
+// Design Name: 
+// Module Name: testbench1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module testbench1(
+    );
+    
+    logic [7:0]inputData, poly, crc, checksum;
+    
+    CRC_Encoder dut (inputData, poly, checksum);
+    
+    initial begin
+    
+    inputData = 0; poly = 0; crc = 0; checksum = 0; #5;
+    
+    inputData = 8'h1a;
+    poly = 8'h9b; #5; 
+    
+    #100;
+    inputData = 8'h11; #5;
+    //#100;
+    //inputData = 8'h1b; #5;
+    //#100;
+   // inputData = 8'h1c; #5;
+    
+    end
+    
+initial
+    
+$monitor( "inputData=%b, poly=%b, crc=%b, checksum=%b", inputData, poly, crc, checksum);
+    
+endmodule
